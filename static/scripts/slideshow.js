c3859f5003c7e25657663847f96bb05f1b35b4c6
austin <austin.cheyao.chen@gmail.com>
2024-10-06 15:37:39 +0800

    Refactor slideshow buttons and styles

--- static/scripts/slideshow.js
+++ static/scripts/slideshow.js
@@ -15,14 +15,14 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) { slideIndex = slides.length }
+    if (n < 1) { slideIndex = 1 }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" slide_active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    dots[slideIndex - 1].className += " slide_active";
 }
